ce11c1b02dcbe982e2bb32e4038c0a1bee870e30
ect (or 2du) <min08101@naver.com>
2018-03-27 00:52:05 +0900

    이렇게 처리하자

--- app.py
+++ app.py
@@ -59,12 +59,19 @@ curs.execute("create table if not exists stats(id text, count text)")
 conn.commit()
 
 # 버전 정리
-bot_version = '다용도봇 v0.0.2'
+bot_version = '다용도봇 v0.0.3'
 print(bot_version)
 
 # URL 인코딩 함수
 def url_encode(data):
     return parse.quote(data).replace('/','%2F')
+
+# 시간 가져오기
+def get_time():
+    now = time.localtime()
+    date = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+
+    return date
 
 def insert_stats(data):
     curs.execute('select count from stats where id = ?', [data])
@@ -81,58 +88,59 @@ def insert_stats(data):
 def tool_send(bot, update):
     # 내용 출력
     print(str(update.message.text))
-    print(str(update.message.date))
-
-    # 챗 아이디
-    chat_id = update.message.chat_id
+    print(int(re.sub('-|:| ', '', str(get_time()))) - int(re.sub('-|:| ', '', str(update.message.date))))
     
-    # 만약 ^\[버전]$ 이 있으면
-    if re.search('^\[버전]$', str(update.message.text)):
-        # 버전을 리턴
-        update.message.reply_text(bot_version)
+    if int(re.sub('-|:| ', '', str(get_time()))) - int(re.sub('-|:| ', '', str(update.message.date))) > 500:
+        # 챗 아이디
+        chat_id = update.message.chat_id
+        
+        # 만약 ^\[버전]$ 이 있으면
+        if re.search('^\[버전]$', str(update.message.text)):
+            # 버전을 리턴
+            update.message.reply_text(bot_version)
 
-    # 만약 ^\[통계]$ 가 있으면
-    if re.search('^\[통계]$', str(update.message.text)):
-        # 통계를 리턴
-        curs.execute("select id, count from stats")
-        count = curs.fetchall()
-        if count:
-            data = ''
-            for plus_data in count:
-                data += '* ' + plus_data[0] + ' : ' + plus_data[1] + '\n'
-        else:
-            data = '* 통계 없음'
+        # 만약 ^\[통계]$ 가 있으면
+        if re.search('^\[통계]$', str(update.message.text)):
+            # 통계를 리턴
+            curs.execute("select id, count from stats")
+            count = curs.fetchall()
+            if count:
+                data = ''
+                for plus_data in count:
+                    data += '* ' + plus_data[0] + ' : ' + plus_data[1] + '\n'
+            else:
+                data = '* 통계 없음'
 
-        update.message.reply_text(data)
+            update.message.reply_text(data)
 
-    # 만약 ^\[도움]$ 이 있으면
-    if re.search('^\[도움]$', str(update.message.text)):
-        # 도움말 리턴
-        update.message.reply_text('== 지원하는 커맨드 ==\n> [[위키명:문서명]]\n>> 나무위키, 리브레위키, 위키백과, 구스위키, 진보위키, 백괴사전\n> [버전]\n> [통계]')
+        # 만약 ^\[도움]$ 이 있으면
+        if re.search('^\[도움]$', str(update.message.text)):
+            # 도움말 리턴
+            update.message.reply_text('== 지원하는 커맨드 ==\n> [[위키명:문서명]]\n>> 나무위키, 리브레위키, 위키백과, 구스위키, 진보위키, 백괴사전\n> [버전]\n> [통계]')
 
-    # 인터위키 내용을 포함하면
-    inter = re.search('^\[\[((?:(?!]]).)+)]]$', str(update.message.text))
-    if inter:
-        # 인터위키 통계 삽입
-        insert_stats('inter')
+        # 인터위키 내용을 포함하면
+        inter = re.search('^\[\[((?:(?!]]).)+)]]$', str(update.message.text))
+        if inter:
+            # 인터위키 통계 삽입
+            insert_stats('inter')
 
-        # 그룹화 해서
-        inter = inter.groups()
+            # 그룹화 해서
+            inter = inter.groups()
 
-        # 다시 사이트 부분과 문서명을 분리
-        start = re.search('^([^:]*):(.*)$', inter[0])
-        if start:
-            # 다시 그룹화
-            start = start.groups()
-            
-            # 만약 사이트가 딕셔너리 안에 있으면
-            if start[0] in link:
-                # 인코딩하고 마크다운으로 리턴
-                bot.send_message(
-                    chat_id = chat_id, 
-                    text = "[" + inter[0] + "](" + link[start[0]] + url_encode(start[1]) + ")", 
-                    parse_mode = telegram.ParseMode.MARKDOWN
-                )
+            # 다시 사이트 부분과 문서명을 분리
+            start = re.search('^([^:]*):(.*)$', inter[0])
+            if start:
+                # 다시 그룹화
+                start = start.groups()
+                
+                # 만약 사이트가 딕셔너리 안에 있으면
+                if start[0] in link:
+                    # 인코딩하고 마크다운으로 리턴
+                    bot.send_message(
+                        chat_id = chat_id, 
+                        text = "[" + inter[0] + "](" + link[start[0]] + url_encode(start[1]) + ")", 
+                        parse_mode = telegram.ParseMode.MARKDOWN
+                    )
 
 # 이 정규식을 포함하는 채팅만 인식하도록
 data_list = ['^\[(?:버전|통계|도움)]$', '^\[\[((?:(?!]]).)+)]]$']
