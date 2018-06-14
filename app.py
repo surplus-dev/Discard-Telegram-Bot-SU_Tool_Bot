4e153d9f4adf01111bfe8a383da6cb2d3ccac110
Surplus_Up (2DU) <min08101@naver.com>
2018-06-14 21:42:04 +0900

    타임존 기능 추가

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 import telegram
 from telegram.ext import Updater, RegexHandler, CommandHandler
 
-from urllib import parse
-
+import urllib.parse
 import sqlite3
 import requests
 import json
+import pytz
 import time
 import re
 
@@ -79,12 +79,12 @@ if not curs.fetchall():
     conn.commit()
 
 # 버전 정리
-bot_version = '다용도봇-06'
+bot_version = '다용도봇-07'
 print(bot_version)
 
 # URL 인코딩 함수
 def url_encode(data):
-    return parse.quote(data).replace('/','%2F')
+    return urllib.parse.quote(data).replace('/','%2F')
 
 # 시간 가져오기
 def get_time():
@@ -111,31 +111,21 @@ def tool_send(bot, update):
     print(delay_time)
 
     if delay_time < 120:        
-        # 챗 아이디
         chat_id = update.message.chat_id
 
-        # 만약 ^\[핑]$ 이 있으면
         if re.search('^\[핑]$', str(update.message.text)):
-            # 통계 삽입
             insert_db('ping')
 
-            # 도움말 리턴
             update.message.reply_text(str(delay_time) + '초')
         
-        # 만약 ^\[버전]$ 이 있으면
         if re.search('^\[버전]$', str(update.message.text)):
-            # 통계 삽입
             insert_db('version')
             
-            # 버전을 리턴
             update.message.reply_text(bot_version)
 
-        # 만약 ^\[통계]$ 가 있으면
         if re.search('^\[통계]$', str(update.message.text)):
-            # 통계 삽입
             insert_db('count')
 
-            # 통계를 리턴
             curs.execute("select id, count from stats order by id asc")
             count = curs.fetchall()
             if count:
@@ -147,7 +137,6 @@ def tool_send(bot, update):
 
             update.message.reply_text(data)
             
-        # 만약 ^\[통계]$ 가 있으면
         re_set = re.search('^\[통계 초기화\(((?:(?!\)).)+)\)]$', str(update.message.text))
         if re_set:
             re_set = re_set.groups()[0]
@@ -159,48 +148,44 @@ def tool_send(bot, update):
 
                 update.message.reply_text('> 완료')
             
-        # 만약 ^\[도움]$ 이 있으면
+        time_re = re.search('^\[시간대\(((?:(?!\)).)+)\)]$', str(update.message.text))
+        if time_re:
+            time_re = time_re.groups()[0]
+
+            try:
+                update.message.reply_text(pytz.timezone(time_re).localize(datetime.datetime.today()).strftime("%Y-%m-%d %H:%M:%S"))
+            except:
+                update.message.reply_text('http://technote.kr/202\n\n타임존 정보')
+
         if re.search('^\[도움]$', str(update.message.text)):
-            # 통계 삽입
             insert_db('help')
 
-            # 도움말 리턴
             update.message.reply_text('= 지원하는 커맨드 =\n== [[위키명:문서명]] ==\n=== 위키 ===\n> 나무, 리브레, 리베, 위백, 구스, 진보, 백괴, 유리\n\n=== 일반 ===\n> 네이버, 구글, 유튜브, 다음\n\n== 기타 ==\n> [버전]\n> [통계]')
 
-        # 인터위키 내용을 포함하면
         inter = re.search('^\[\[((?:(?!]]).)+)]]$', str(update.message.text))
         if inter:
-            # 인터위키 통계 삽입
             insert_db('inter')
 
-            # 그룹화 해서
             inter = inter.groups()
 
-            # 다시 사이트 부분과 문서명을 분리
             start = re.search('^([^:]*):(.*)$', inter[0])
             if start:
-                # 다시 그룹화
                 start = start.groups()
                 
-                # 만약 사이트가 딕셔너리 안에 있으면
                 if start[0] in link:
-                    # 세부적인 통계 삽입
                     insert_db('inter:' + start[0])
 
-                    # 404 확인
                     if requests.get(link[start[0]] + url_encode(start[1])).status_code != 404:
-                        # 인코딩하고 마크다운으로 리턴
                         bot.send_message(
                             chat_id = chat_id, 
                             text = "[" + inter[0] + "](" + link[start[0]] + url_encode(start[1]) + ")", 
-                            parse_mode = telegram.ParseMode.MARKDOWN
+                            urllib.parse_mode = telegram.urllib.parseMode.MARKDOWN
                         )
                     else:
-                        # 404면 문서 없음
                         bot.send_message(
                             chat_id = chat_id, 
                             text = "문서가 없습니다.\n\n> [구글](https://www.google.com/search?q=" + start[0] + ' ' + url_encode(start[1]) + ")\n> [덕덕고](https://duckduckgo.com/?q=" + start[0] + ' ' + url_encode(start[1]) + ")", 
-                            parse_mode = telegram.ParseMode.MARKDOWN
+                            urllib.parse_mode = telegram.urllib.parseMode.MARKDOWN
                         )
 
 # 이 정규식을 포함하는 채팅만 인식하도록
