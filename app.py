9fc883d11d532b9098383beedc517e1eefb5f33f
Surplus_Up (2DU) <min08101@naver.com>
2018-06-16 16:12:44 +0900

    버그 수정

--- app.py
+++ app.py
@@ -98,98 +98,100 @@ def insert_db(data):
     conn.commit()
     
 def tool_send(bot, update):
-    print(str(update.message.text))
+    try:
+        print('text : ' + str(update.message.text))
 
-    delay_time = int(re.sub('-|:| ', '', get_time())) - int(re.sub('-|:| ', '', str(update.message.date)))
-    print(delay_time)
+        delay_time = int(re.sub('-|:| ', '', get_time())) - int(re.sub('-|:| ', '', str(update.message.date)))
+        print('time : ' + str(delay_time))
 
-    if delay_time < 120:        
-        chat_id = update.message.chat_id
+        if delay_time < 120:        
+            chat_id = update.message.chat_id
 
-        if re.search('^\[핑]$', str(update.message.text)):
-            insert_db('ping')
+            if re.search('^\[핑]$', str(update.message.text)):
+                insert_db('ping')
 
-            update.message.reply_text(str(delay_time) + '초')
-        
-        if re.search('^\[버전]$', str(update.message.text)):
-            insert_db('version')
+                update.message.reply_text(str(delay_time) + '초')
             
-            update.message.reply_text(bot_version)
+            if re.search('^\[버전]$', str(update.message.text)):
+                insert_db('version')
+                
+                update.message.reply_text(bot_version)
 
-        if re.search('^\[통계]$', str(update.message.text)):
-            insert_db('count')
+            if re.search('^\[통계]$', str(update.message.text)):
+                insert_db('count')
 
-            curs.execute("select id, count from stats order by id asc")
-            count = curs.fetchall()
-            if count:
-                data = ''
-                for plus_data in count:
-                    data += '> ' + plus_data[0] + ' : ' + plus_data[1] + '\n'
-            else:
-                data = '> 통계 없음'
+                curs.execute("select id, count from stats order by id asc")
+                count = curs.fetchall()
+                if count:
+                    data = ''
+                    for plus_data in count:
+                        data += '> ' + plus_data[0] + ' : ' + plus_data[1] + '\n'
+                else:
+                    data = '> 통계 없음'
 
-            update.message.reply_text(data)
-            
-        re_set = re.search('^\[통계 초기화\(((?:(?!\)).)+)\)]$', str(update.message.text))
-        if re_set:
-            re_set = re_set.groups()[0]
+                update.message.reply_text(data)
+                
+            re_set = re.search('^\[통계 초기화\(((?:(?!\)).)+)\)]$', str(update.message.text))
+            if re_set:
+                re_set = re_set.groups()[0]
 
-            curs.execute("select data from setting where id = 'pw'")
-            if re_set == curs.fetchall()[0][0]:
-                curs.execute('delete from stats')
-                conn.commit()
+                curs.execute("select data from setting where id = 'pw'")
+                if re_set == curs.fetchall()[0][0]:
+                    curs.execute('delete from stats')
+                    conn.commit()
 
-                update.message.reply_text('> 완료')
-            
-        time_re = re.search('^\[시간대\(((?:(?!\)).)+)\)]$', str(update.message.text))
-        if time_re:
-            time_re = time_re.groups()[0]
+                    update.message.reply_text('> 완료')
+                
+            time_re = re.search('^\[시간대\(((?:(?!\)).)+)\)]$', str(update.message.text))
+            if time_re:
+                time_re = time_re.groups()[0]
 
-            try:
+                now_date = re.findall('([0-9]+)', str(update.message.date))
+
                 update.message.reply_text(pytz.timezone('Asia/Seoul').localize(
                         datetime.datetime(
-                            int(datetime.datetime.today().strftime("%Y")),
-                            int(datetime.datetime.today().strftime("%m")),
-                            int(datetime.datetime.today().strftime("%d")),
-                            int(datetime.datetime.today().strftime("%H")),
-                            int(datetime.datetime.today().strftime("%M")),
-                            int(datetime.datetime.today().strftime("%S"))
+                            int(now_date[0]),
+                            int(now_date[1]),
+                            int(now_date[2]),
+                            int(now_date[3]),
+                            int(now_date[4]),
+                            int(now_date[5]),
                         )
                     ).astimezone(pytz.timezone(tz_lookup[time_re] if time_re in tz_lookup else time_re)).strftime("%Y-%m-%d %H:%M:%S %Z%z")
                 )
-            except:
-                update.message.reply_text('https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568\n\n타임존 정보')
 
-        if re.search('^\[도움]$', str(update.message.text)):
-            insert_db('help')
+            if re.search('^\[도움]$', str(update.message.text)):
+                insert_db('help')
 
-            update.message.reply_text('= 지원하는 커맨드 =\n== [[위키명:문서명]] ==\n=== 위키 ===\n> 나무, 리브레, 리베, 위백, 구스, 진보, 백괴, 유리\n\n=== 일반 ===\n> 네이버, 구글, 유튜브, 다음\n\n== 기타 ==\n> [버전]\n> [통계]')
+                update.message.reply_text('= 지원하는 커맨드 =\n== [[위키명:문서명]] ==\n=== 위키 ===\n> 나무, 리브레, 리베, 위백, 구스, 진보, 백괴, 유리\n\n=== 일반 ===\n> 네이버, 구글, 유튜브, 다음\n\n== 기타 ==\n> [버전]\n> [통계]')
 
-        inter = re.search('^\[\[((?:(?!]]).)+)]]$', str(update.message.text))
-        if inter:
-            insert_db('inter')
+            inter = re.search('^\[\[((?:(?!]]).)+)]]$', str(update.message.text))
+            if inter:
+                insert_db('inter')
 
-            inter = inter.groups()
+                inter = inter.groups()
 
-            start = re.search('^([^:]*):(.*)$', inter[0])
-            if start:
-                start = start.groups()
-                
-                if start[0] in link:
-                    insert_db('inter:' + start[0])
+                start = re.search('^([^:]*):(.*)$', inter[0])
+                if start:
+                    start = start.groups()
+                    
+                    if start[0] in link:
+                        insert_db('inter:' + start[0])
 
-                    if requests.get(link[start[0]] + url_encode(start[1])).status_code != 404:
-                        bot.send_message(
-                            chat_id = chat_id, 
-                            text = "[" + inter[0] + "](" + link[start[0]] + url_encode(start[1]) + ")", 
-                            parse_mode = telegram.urllib.parseMode.MARKDOWN
-                        )
-                    else:
-                        bot.send_message(
-                            chat_id = chat_id, 
-                            text = "문서가 없습니다.\n\n> [구글](https://www.google.com/search?q=" + start[0] + ' ' + url_encode(start[1]) + ")\n> [덕덕고](https://duckduckgo.com/?q=" + start[0] + ' ' + url_encode(start[1]) + ")", 
-                            parse_mode = telegram.urllib.parseMode.MARKDOWN
-                        )
+                        if requests.get(link[start[0]] + url_encode(start[1])).status_code != 404:
+                            bot.send_message(
+                                chat_id = chat_id, 
+                                text = "[" + inter[0] + "](" + link[start[0]] + url_encode(start[1]) + ")", 
+                                parse_mode = 'Markdown'
+                            )
+                        else:
+                            bot.send_message(
+                                chat_id = chat_id, 
+                                text = "문서가 없습니다.\n\n> [구글](https://www.google.com/search?q=" + start[0] + ' ' + url_encode(start[1]) + ")\n> [덕덕고](https://duckduckgo.com/?q=" + start[0] + ' ' + url_encode(start[1]) + ")", 
+                                parse_mode = 'Markdown'
+                            )
+    except Exception as e:
+        print('Error : ' + str(e))
 
 # 이 정규식을 포함하는 채팅만 인식하도록
 data_list = ['^\[(?:((?!\]).)+)]$', '^\[\[((?:(?!]]).)+)]]$']
