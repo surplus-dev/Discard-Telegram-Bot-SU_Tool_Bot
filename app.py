486bde2bf24cff7123aa03f2657746257afd2a85
Surplus_Up (2DU) <min08101@naver.com>
2020-02-02 16:09:17 +0900

    버그 수정, 좀 더 효율적인 방법

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if not curs.fetchall():
     curs.execute('insert into setting (id, data) values ("pw", ?)', [pw])
     conn.commit()
 
-bot_version = '다용도봇-11'
+bot_version = '다용도봇-12'
 print(bot_version)
 
 def url_encode(data):
@@ -100,176 +100,162 @@ def get_zip(data):
     
 def tool_send(update, context):
     try:
-        main_data = str(update.message.text)
-        main_data = re.sub('\[br\]', '<br>', main_data)
-        if re.search('(?:\[(?:((?!\]).)+)]|\[\[((?:(?!]]).)+)]])', main_data):
-            print('text : ' + str(update.message.text))
-
-            chat_id = update.message.chat_id
-            sender = update.message.from_user.id
-            run_int = 1
+        chat_id = update.message.chat_id
+        sender = update.message.from_user.id
+        run_int = 1
             
-            while 1:            
-                if re.search('(?:\[(?:((?!\]).)+)]|\[\[((?:(?!]]).)+)]])', main_data):
-                    print('run : ' + str(run_int))
-                    
-                    if re.search('\[핑]', main_data):
-                        insert_db('ping')
-                    
-                    if re.search('\[버전]', main_data):
-                        insert_db('version')
-                        
-                        update.message.reply_text(bot_version)
+        all_data = re.findall('(\[(?:(?:(?!\]).)+)]|\[\[(?:(?:(?!]]).)+)]])', str(update.message.text))
+        for main_data in all_data:
+            print('run : ' + str(run_int))
+            
+            if re.search('\[핑]', main_data):
+                insert_db('ping')
+            
+            if re.search('\[버전]', main_data):
+                insert_db('version')
+                
+                update.message.reply_text(bot_version)
 
-                    if re.search('\[통계]', main_data):
-                        insert_db('count')
+            if re.search('\[통계]', main_data):
+                insert_db('count')
 
-                        curs.execute("select id, count from stats order by id asc")
-                        count = curs.fetchall()
-                        if count:
-                            data = ''
-                            for plus_data in count:
-                                data += '> ' + plus_data[0] + ' : ' + plus_data[1] + '\n'
-                        else:
-                            data = '> 통계 없음'
-
-                        update.message.reply_text(data)
-                        
-                    re_set = re.search('\[통계 초기화\(((?:(?!\)).)+)\)]', main_data)
-                    if re_set:
-                        re_set = re_set.groups()[0]
-
-                        curs.execute("select data from setting where id = 'pw'")
-                        if re_set == curs.fetchall()[0][0]:
-                            curs.execute('delete from stats')
-                            conn.commit()
-
-                            update.message.reply_text('> 완료')
-
-                    set_data = re.search('\[설정\(([^,]+), ?([^,]+)\)]', main_data)
-                    if set_data:
-                        set_data = set_data.groups()
-                        
-                        insert_db('set')
-
-                        ok_list = ['wiki']
-                        if set_data[0] in ok_list:
-                            curs.execute('select id from user_set where id = ? and user = ?', [set_data[0], sender])
-                            if curs.fetchall():
-                                curs.execute("update user_set set data = ? where id = ? and user = ?", [set_data[1], set_data[0], sender])
-                            else:
-                                curs.execute('insert into user_set (id, user, data) values (?, ?, ?)', [set_data[0], sender, set_data[1]])
-
-                        conn.commit()
-
-                        update.message.reply_text('> 완료')
-
-                    if re.search('\[도움]', main_data):
-                        insert_db('help')
-
-                        context.bot.send_message(
-                            chat_id = chat_id,
-                            text =  get_zip(
-                                        '''
-                                        == `[[위키명:문서명]]` ==\n
-                                        === 위키 ===\n
-                                        > 나무, 리브레, 리베, 디키, 위백, 구스, 진보, 백괴, 유리\n\n
-                                        === 일반 ===\n
-                                        > 네이버, 구글, 유튜브, 다음\n\n
-                                        == `[설명(이름, 값)]` ==\n
-                                        > `wiki, 위키명` (인터위키 기본값)\n\n
-                                        == 기타 ==\n
-                                        > `[버전]`\n
-                                        > `[통계]`\n
-                                        > `[핑]`
-                                        '''
-                                    ),
-                            parse_mode = 'Markdown'
-                        )
-
-                    inter = re.search('\[\[((?:(?!]]).)+)]]', main_data)
-                    if inter:
-                        insert_db('inter')
-
-                        inter = inter.groups()
-
-                        curs.execute('select data from user_set where id = "wiki" and user = ?', [sender])
-                        data = curs.fetchall()
-
-                        start = re.search('([^:]*):(.*)', inter[0])
-                        if start or data:
-                            if start:
-                                start = start.groups()
-                            else:
-                                start = [data[0][0], inter[0]]
-                            
-                            if not start[0] in link:
-                                if data[0][0] in link:
-                                    start = [data[0][0], inter[0]]
-                                    pass_num = 1
-                                else:
-                                    pass_num = 0
-                            else:
-                                pass_num = 1
-
-                            if pass_num == 1:
-                                insert_db('inter:' + start[0])
-
-                                try:
-                                    if start[0] != '히토미':
-                                        if start[0] != 'SCP':
-                                            data = link[start[0]] + url_encode(start[1])
-                                        else:
-                                            data = link[start[0]] + url_encode(start[1]).replace('%2F', '/')
-
-                                        if requests.get(data).status_code != 404:
-                                            link_go = data
-
-                                            context.bot.send_message(
-                                                chat_id = chat_id, 
-                                                text = "[" + start[0] + ":" + start[1] + "](" + link_go + ")",
-                                                parse_mode = 'Markdown'
-                                            )
-                                        else:
-                                            data_link = re.search('^https?:\/\/([^/]+)', link[start[0]])
-                                            data_link = data_link.groups()[0]
-
-                                            context.bot.send_message(
-                                                chat_id = chat_id, 
-                                                text = get_zip(
-                                                            '''
-                                                            ''' + start[0] + ''' 문서가 없습니다.\n\n
-                                                            > [구글](https://www.google.com/search?q=site:''' + data_link + ' ' + url_encode(start[1]) + ''')\n
-                                                            > [덕덕고](https://duckduckgo.com/?q=site:''' + data_link + ' ' + url_encode(start[1]) + ''')
-                                                            '''
-                                                        ),
-                                                parse_mode = 'Markdown'
-                                            )
-                                    else:
-                                        link_go = link[start[0]] + url_encode(start[1]) + '.html'
-
-                                        context.bot.send_message(
-                                            chat_id = chat_id, 
-                                            text = "[" + start[0] + ":" + start[1] + "](" + link_go + ")",
-                                            parse_mode = 'Markdown'
-                                        )
-                                except:
-                                    context.bot.send_message(
-                                            chat_id = chat_id, 
-                                            text = get_zip(
-                                                        '''
-                                                        연결 실패.\n\n
-                                                        > [구글](https://www.google.com/search?q=''' + start[0] + ' ' + url_encode(start[1]) + ''')\n
-                                                        > [덕덕고](https://duckduckgo.com/?q=''' + start[0] + ' ' + url_encode(start[1]) + ''')
-                                                        '''
-                                                    ), 
-                                            parse_mode = 'Markdown'
-                                        )
-                                    
-                    main_data = re.sub('(?:\[(?:((?!\]).)+)]|\[\[((?:(?!]]).)+)]])', '', main_data, 1)
-                    run_int += 1
+                curs.execute("select id, count from stats order by id asc")
+                count = curs.fetchall()
+                if count:
+                    data = ''
+                    for plus_data in count:
+                        data += '> ' + plus_data[0] + ' : ' + plus_data[1] + '\n'
                 else:
-                    break
+                    data = '> 통계 없음'
+
+                update.message.reply_text(data)
+                
+            re_set = re.search('\[통계 초기화\(((?:(?!\)).)+)\)]', main_data)
+            if re_set:
+                re_set = re_set.groups()[0]
+
+                curs.execute("select data from setting where id = 'pw'")
+                if re_set == curs.fetchall()[0][0]:
+                    curs.execute('delete from stats')
+                    conn.commit()
+
+                    update.message.reply_text('> 완료')
+
+            set_data = re.search('\[설정\(([^,]+), ?([^,]+)\)]', main_data)
+            if set_data:
+                set_data = set_data.groups()
+                
+                insert_db('set')
+
+                ok_list = ['wiki']
+                if set_data[0] in ok_list:
+                    curs.execute('select id from user_set where id = ? and user = ?', [set_data[0], sender])
+                    if curs.fetchall():
+                        curs.execute("update user_set set data = ? where id = ? and user = ?", [set_data[1], set_data[0], sender])
+                    else:
+                        curs.execute('insert into user_set (id, user, data) values (?, ?, ?)', [set_data[0], sender, set_data[1]])
+
+                conn.commit()
+
+                update.message.reply_text('> 완료')
+
+            if re.search('\[도움]', main_data):
+                insert_db('help')
+
+                context.bot.send_message(
+                    chat_id = chat_id,
+                    text =  get_zip('''
+                        == `[[위키명:문서명]]` ==\n
+                        === 위키 ===\n
+                        > 나무, 리브레, 리베, 디키, 위백, 구스, 진보, 백괴, 유리\n\n
+                        === 일반 ===\n
+                        > 네이버, 구글, 유튜브, 다음\n\n
+                        == `[설명(이름, 값)]` ==\n
+                        > `wiki, 위키명` (인터위키 기본값)\n\n
+                        == 기타 ==\n
+                        > `[버전]`\n
+                        > `[통계]`\n
+                        > `[핑]`
+                    '''),
+                    parse_mode = 'Markdown'
+                )
+
+            inter = re.search('\[\[((?:(?!]]).)+)]]', main_data)
+            if inter:
+                insert_db('inter')
+
+                inter = inter.groups()
+
+                curs.execute('select data from user_set where id = "wiki" and user = ?', [sender])
+                data = curs.fetchall()
+
+                start = re.search('([^:]*):(.*)', inter[0])
+                if start or data:
+                    if start:
+                        start = start.groups()
+                    else:
+                        start = [data[0][0], inter[0]]
+                    
+                    if not start[0] in link:
+                        if data[0][0] in link:
+                            start = [data[0][0], inter[0]]
+                            pass_num = 1
+                        else:
+                            pass_num = 0
+                    else:
+                        pass_num = 1
+
+                    if pass_num == 1:
+                        insert_db('inter:' + start[0])
+
+                        try:
+                            if start[0] != '히토미':
+                                if start[0] != 'SCP':
+                                    data = link[start[0]] + url_encode(start[1])
+                                else:
+                                    data = link[start[0]] + url_encode(start[1]).replace('%2F', '/')
+
+                                if requests.get(data).status_code != 404:
+                                    link_go = data
+
+                                    context.bot.send_message(
+                                        chat_id = chat_id, 
+                                        text = "[" + start[0] + ":" + start[1] + "](" + link_go + ")",
+                                        parse_mode = 'Markdown'
+                                    )
+                                else:
+                                    data_link = re.search('^https?:\/\/([^/]+)', link[start[0]])
+                                    data_link = data_link.groups()[0]
+
+                                    context.bot.send_message(
+                                        chat_id = chat_id, 
+                                        text = get_zip(
+                                            start[0] + ''' 문서가 없습니다.\n\n
+                                            > [구글](https://www.google.com/search?q=site:''' + data_link + ' ' + url_encode(start[1]) + ''')\n
+                                            > [덕덕고](https://duckduckgo.com/?q=site:''' + data_link + ' ' + url_encode(start[1]) + ''')
+                                        '''),
+                                        parse_mode = 'Markdown'
+                                    )
+                            else:
+                                link_go = link[start[0]] + url_encode(start[1]) + '.html'
+
+                                context.bot.send_message(
+                                    chat_id = chat_id, 
+                                    text = "[" + start[0] + ":" + start[1] + "](" + link_go + ")",
+                                    parse_mode = 'Markdown'
+                                )
+                        except:
+                            context.bot.send_message(
+                                chat_id = chat_id, 
+                                text = get_zip('''
+                                    연결 실패.\n\n
+                                    > [구글](https://www.google.com/search?q=''' + start[0] + ' ' + url_encode(start[1]) + ''')\n
+                                    > [덕덕고](https://duckduckgo.com/?q=''' + start[0] + ' ' + url_encode(start[1]) + ''')
+                                '''), 
+                                parse_mode = 'Markdown'
+                            )
+
+            run_int += 1
     except Exception as e:
         print('Error : ' + str(e))
     
